4db60921ee7c15fdae68282da0cee80671fb1dc4
Mahmoud Kamel <kamelmahmoud335@gmail.com>
2025-10-04 00:14:47 +0300

    add commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,12 @@
-export default {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'chore', 'docs', 'style', 'test']],
-        'scope-empty': [2, 'never'], // لازم يبقى فيه scope
-        'scope-case': [2, 'always', 'kebab-case'],
-        'subject-empty': [2, 'never'], // لازم فيه شرح
-        'subject-case': [0], // مش هنقيد الكابيتالايزيشن
-    },
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'chore', 'docs', 'style', 'test']],
+    'scope-empty': [2, 'never'],
+    'scope-case': [2, 'always', 'kebab-case'],
+    'subject-empty': [2, 'never'],
+    'subject-case': [0],
+  },
 };
+
+export default config;
