a4d7dfbe6a46766c3076acf430a8c6d9b52dbd99
Mahmoud Hamed <mahmoud.hamed.shenawy@gmail.com>
2025-10-07 00:28:35 +0300

    feat(highlighted_heading): create reusable HighlightedHeading component with dark mode support

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,11 +1,14 @@
+import HighlightedHeading from "@components/shared/highlighted-heading";
 import Subtitle from "@components/shared/subtitle";
+import { cn } from "@lib/utils/cn.utils";
 
 export default function HomePage() {
     return (
         <>
-           <div className="p-4 text-center">
-             <Subtitle text="Gallery" />
-           </div>
+            <div className={cn("flex flex-col items-center gap-2 p-4 text-center")}>
+                <Subtitle text="Gallery" />
+                <HighlightedHeading text="Check Out our Wonderful Gallery" highlightWidth="350px" borderWidth="150px" />
+            </div>
         </>
     );
 }
