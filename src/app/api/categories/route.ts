4341a3eb0289748863fbb6717283ea0de160b3f7
Mahmoud Hamed <mahmoud.hamed.shenawy@gmail.com>
2025-10-21 14:05:23 +0300

    chore: add comments  for better readability

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -2,26 +2,31 @@ import { CategoriesResponse } from "@app/[locale]/products/_types/categories";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
+    // Parse URL to get query parameters
     const { searchParams } = new URL(req.url);
+    // Default values if parameters are not provided
     const page = searchParams.get("page") || "1";
     const limit = searchParams.get("limit") || "10";
 
     try {
+        // Fetch categories from the external API
         const res = await fetch(`https://flower.elevateegy.com/api/v1/categories?page=${page}&limit=${limit}`, {
             headers: {
                 "Content-Type": "application/json",
             },
             cache: "no-store",
         });
-
+        // Handle non-OK responses
         if (!res.ok) {
             return NextResponse.json({ message: "Failed to fetch categories" }, { status: 500 });
         }
 
+        // Parse and return the JSON response
         const data: CategoriesResponse = await res.json();
         return NextResponse.json(data);
     } catch (error) {
-        console.error(error);
+        // Handle fetch errors
+
         return NextResponse.json({ message: "error", categories: [] }, { status: 500 });
     }
 }
