c0bd5a2791df27241742ccdf5bcb7f59283b7606
Ahmed Ahmed <ahmed5754egypt@gmail.com>
2025-10-25 17:20:51 +0300

    change(productId): change the route name

--- src/app/[locale]/products/[productId]/page.tsx
+++ src/app/[locale]/products/[productId]/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ProductDetailes from "./_components/product-detailes";
 
-export default async function ProductId({ params }: { params: { "product-id": string } }) {
-    const { "product-id": productId } = params;
+export default async function ProductId({ params }: { params: { productId: string } }) {
+    const { productId } = params;
 
     return (
         <main className="px-20 py-16">
