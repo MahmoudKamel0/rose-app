a085399f2ea198d1c44be4a54245a6ddec27425d
Mahmoud Hamed <mahmoud.hamed.shenawy@gmail.com>
2025-10-29 12:36:04 +0300

    Add translations for About, Gallery, and Companies sections

--- src/components/features/application/home/about-section/components/about-content.tsx
+++ src/components/features/application/home/about-section/components/about-content.tsx
@@ -1,31 +1,31 @@
 import Subtitle from "@components/shared/subtitle";
 import AboutFeatures from "./about-features";
 import BtnPrimary from "@components/shared/btn-primary";
+import { useTranslations } from "next-intl";
 
 export default function AboutContent() {
+    const t = useTranslations("about-section");
     return (
         <div className="flex-1">
             {/* About Heading */}
             <div className="about-heading mt-5 flex flex-col gap-6 pt-0.5">
-                <Subtitle text="about" />
+                <Subtitle text={t("subtitle")} />
 
                 <h2 className="text-3xl font-bold capitalize text-[#741C21] dark:text-[#FFC2D0]">
-                    delivering the <span className="text-[#FF668B] dark:text-[#D75458]">finest</span> gift boxes for your <br />{" "}
-                    <span className="text-[#FF668B] dark:text-[#D75458]">special</span> moments
+                    {t("heading.part1")} <span className="text-[#FF668B] dark:text-[#D75458]"> {t("heading.highlight1")}</span>{" "}
+                    {t("heading.part2")} <br /> <span className="text-[#FF668B] dark:text-[#D75458]"> {t("heading.highlight2")}</span>{" "}
+                    {t("heading.part3")}
                 </h2>
             </div>
             {/* About Description */}
             <div className="about-description">
                 <p className="mt-2 leading-none tracking-[0em] text-zinc-500 dark:text-[#C4C4C4]">
-                    Make every moment memorable with our premium gift boxes. Carefully curated and <br /> beautifully packaged, each box is
-                    filled with handpicked items designed to impress.
-                    <br /> Whether it's for a birthday, wedding, or a simple “thank you,” our gift boxes are crafted to <br /> leave a
-                    lasting impression — because thoughtful gifting starts here.
+                    <span dangerouslySetInnerHTML={{ __html: t("description") }} />
                 </p>
             </div>
             {/* DiscoverButton  */}
             <div className="mt-9">
-                <BtnPrimary text="Discover" />
+                <BtnPrimary text={t("button")} />
             </div>
              {/* AboutFeatures  */}
             <AboutFeatures />
