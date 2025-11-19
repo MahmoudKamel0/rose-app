b9032dc33f273a5be264dd1560481d79a3d546dd
Mahmoud Hamed <mahmoud.hamed.shenawy@gmail.com>
2025-11-19 14:02:07 +0300

    feat : implement the loading suspse for the products statistics

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import ProductsStatistics from "./_components/products-statistics";
+import ProductStatisticsLoader from "./_components/product-statistics-loader";
 
 export default function page() {
     return (
         <div className="min-h-screen bg-zinc-50">
-            <ProductsStatistics />
+            <Suspense fallback={<ProductStatisticsLoader />}>
+                <ProductsStatistics />
+            </Suspense>
         </div>
     );
 }
